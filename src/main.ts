614050c3f8ddaa0f050d4d538c3df1f3b47e73a9
Nomad <initdaemon@protonmail.com>
2023-02-12 17:21:53 -0600

    fixed init commit issues

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { SunsetTransModule } from './app/sunset-trans.module';
 
-import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(SunsetTransModule)
   .catch(err => console.error(err));
